447d39434bd6876a1addc6efab8aceb02265fa22
kevingunawanc14 <c14200005@john.petra.ac.id>
2024-03-02 08:36:25 +0700

    feat: improve navbar user for user experience

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,6 +6,12 @@ import { IoTrophyOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { IoCompass } from "react-icons/io5";
+import { PiHeartbeatLight } from "react-icons/pi";
+import { PiHeartbeatFill } from "react-icons/pi";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+import { RiAccountCircleFill } from "react-icons/ri";
+import { IoTrophy } from "react-icons/io5";
 
 export default function Navbar() {
     const navigate = useNavigate();
@@ -25,28 +31,28 @@ export default function Navbar() {
     return (
         <div className="btm-nav mt-10">
             <button className={window.location.pathname === '/' ? 'active text-primary' : ''} onClick={() => handleNavigate('/')}>
-                <IoCompassOutline />
-                <span className="btm-nav-label">Home</span>
+                {window.location.pathname === '/' ? <IoCompass /> : <IoCompassOutline />}
+                <span className={window.location.pathname === '/' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Home</span>
             </button>
 
             <button className={window.location.pathname === '/journal' ? 'active text-primary' : ''} onClick={() => handleNavigate('/journal')}>
-                <LiaHeartbeatSolid />
-                <span className="btm-nav-label">Journal</span>
+                {window.location.pathname === '/journal' ? <PiHeartbeatFill /> : <PiHeartbeatLight />}
+                <span className={window.location.pathname === '/journal' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Journal</span>
             </button>
 
             <button className={window.location.pathname === '/payment' ? 'active text-primary' : ''} onClick={() => handleNavigate('/payment')}>
-                <RiMoneyDollarCircleLine />
-                <span className="btm-nav-label">Payment</span>
+                {window.location.pathname === '/payment' ? <RiMoneyDollarCircleFill /> : <RiMoneyDollarCircleLine />}
+                <span className={window.location.pathname === '/payment' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Payment</span>
             </button>
 
             <button className={window.location.pathname === '/leaderboard' ? 'active text-primary' : ''} onClick={() => handleNavigate('/leaderboard')}>
-                <IoTrophyOutline />
-                <span className="btm-nav-label">Leaderboard</span>
+                {window.location.pathname === '/leaderboard' ? <IoTrophy /> : <IoTrophyOutline />}
+                <span className={window.location.pathname === '/leaderboard' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Leaderboard</span>
             </button>
 
             <button className={window.location.pathname === '/setting' ? 'active text-primary' : ''} onClick={() => handleNavigate('/setting')}>
-                <RiAccountCircleLine />
-                <span className="btm-nav-label">Account</span>
+                {window.location.pathname === '/setting' ? <RiAccountCircleFill /> : <RiAccountCircleLine />}
+                <span className={window.location.pathname === '/setting' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Account</span>
             </button>
         </div >
     );
