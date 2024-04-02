eb52ccd18badf705bb2b8cf1f96717ba1c47ad5b
kevingunawanc14 <c14200005@john.petra.ac.id>
2024-04-02 11:59:36 +0700

    push-up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -30,29 +30,29 @@ export default function Navbar() {
 
     return (
         <div className="btm-nav mt-10">
-            <button className={window.location.pathname === '/' ? 'active text-primary' : ''} onClick={() => handleNavigate('/')}>
+            <button className={`sm:btm-nav-md btm-nav-xs ${window.location.pathname === '/' ? 'active text-primary ' : ''}`} onClick={() => handleNavigate('/')}>
                 {window.location.pathname === '/' ? <IoCompass /> : <IoCompassOutline />}
                 <span className={window.location.pathname === '/' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Home</span>
             </button>
 
-            <button className={window.location.pathname === '/journal' ? 'active text-primary' : ''} onClick={() => handleNavigate('/journal')}>
+            <button className={`sm:btm-nav-md btm-nav-xs ${window.location.pathname === '/journal' ? 'active text-primary ' : ''}`} onClick={() => handleNavigate('/journal')}>
                 {window.location.pathname === '/journal' ? <PiHeartbeatFill /> : <PiHeartbeatLight />}
                 <span className={window.location.pathname === '/journal' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Journal</span>
             </button>
 
-            <button className={window.location.pathname === '/payment' ? 'active text-primary' : ''} onClick={() => handleNavigate('/payment')}>
+            <button className={`sm:btm-nav-md btm-nav-xs ${window.location.pathname === '/payment' ? 'active text-primary ' : ''}`} onClick={() => handleNavigate('/payment')}>
                 {window.location.pathname === '/payment' ? <RiMoneyDollarCircleFill /> : <RiMoneyDollarCircleLine />}
-                <span className={window.location.pathname === '/payment' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Payment</span>
+                <span className={window.location.pathname === '/payment' ? 'btm-nav-label font-medium ' : 'btm-nav-label'}>Payment</span>
             </button>
 
-            <button className={window.location.pathname === '/leaderboard' ? 'active text-primary' : ''} onClick={() => handleNavigate('/leaderboard')}>
+            <button className={`sm:btm-nav-md btm-nav-xs ${window.location.pathname === '/leaderboard' ? 'active text-primary ' : ''}`} onClick={() => handleNavigate('/leaderboard')}>
                 {window.location.pathname === '/leaderboard' ? <IoTrophy /> : <IoTrophyOutline />}
                 <span className={window.location.pathname === '/leaderboard' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Leaderboard</span>
             </button>
 
-            <button className={window.location.pathname === '/setting' ? 'active text-primary' : ''} onClick={() => handleNavigate('/setting')}>
+            <button className={`sm:btm-nav-md btm-nav-xs ${window.location.pathname === '/setting' ? 'active text-primary ' : ''}`} onClick={() => handleNavigate('/setting')}>
                 {window.location.pathname === '/setting' ? <RiAccountCircleFill /> : <RiAccountCircleLine />}
-                <span className={window.location.pathname === '/setting' ? 'btm-nav-label font-medium' : 'btm-nav-label'}>Account</span>
+                <span className={window.location.pathname === '/setting' ? 'btm-nav-label font-medium ' : 'btm-nav-label'}>Account</span>
             </button>
         </div >
     );
--- src/pages/customer/setting.jsx
+++ src/pages/customer/setting.jsx
@@ -123,34 +123,33 @@ export default function DetailAccount() {
                         </div>
 
                     </div>
-                    <div className="collapse-content grid place-items-center  ">
+                    <div className="collapse-content">
                         <div className="grid grid-cols-1">
                             <div className="mt-3">
-                                <kbd className="kbd mx-2 text-sm w-60">Username:  {userData.username}</kbd>
-
+                                <button className="btn btn-block">Username:  {userData.username}</button>
                             </div>
                         </div>
                         <div className="grid grid-cols-1">
                             <div className="mt-3">
-                                <kbd className="kbd mx-2 text-sm  w-60">Rank:  {userData.rank}   {userData.rank === 'Platinum' ? <GiRank2 color="#3ba8ba" fontSize="25px" /> :
+                                <button className="btn btn-block">Rank:  {userData.rank}   {userData.rank === 'Platinum' ? <GiRank2 color="#3ba8ba" fontSize="25px" /> :
                                     userData.rank === 2 ? <GiRank2 color="#a46ced" fontSize="25px" /> :
                                         userData.rank === 3 ? <GiRank2 color="#eccc55" fontSize="25px" /> :
-                                            null} </kbd>
+                                            null}</button>
                             </div>
                         </div>
                         <div className="grid grid-cols-1">
                             <div className="mt-3">
-                                <kbd className="kbd mx-2 text-sm  w-60">Xp: {userData.xp}</kbd>
+                                <button className="btn btn-block">Xp: {userData.xp}</button>
                             </div>
                         </div>
                         <div className="grid grid-cols-1">
                             <div className="mt-3">
-                                <kbd className="kbd mx-2 text-sm w-60">Hp:  {userData.hp}</kbd>
+                                <button className="btn btn-block">Hp:  {userData.hp}</button>
                             </div>
                         </div>
                         <div className="grid grid-cols-1">
                             <div className="mt-3">
-                                <kbd className="kbd mx-2 text-sm  w-60">Krakatau Coin:  {userData.coin}</kbd>
+                                <button className="btn btn-block">Krakatau Coin:  {userData.coin}</button>
                             </div>
                         </div>
 
@@ -168,15 +167,21 @@ export default function DetailAccount() {
                             </div>
                         </div>
                     </div>
-                    <div className="collapse-content grid place-items-center  ">
-                        <div className="mt-3 ">
-                            <kbd className="kbd mx-2 text-sm w-60">Total Minute Workout:  <br /> {userData.totalMinuteWorkout}</kbd>
+                    <div className="collapse-content   ">
+                        <div className="grid grid-cols-1">
+                            <div className="mt-3">
+                                <button className="btn btn-block" >Total Minute Workout:   {userData.totalMinuteWorkout}</button>
+                            </div>
                         </div>
-                        <div className="mt-3">
-                            <kbd className="kbd mx-2 text-sm w-60">Most Sport Played:  <br /> {userData.mostSportPlayed}</kbd>
+                        <div className="grid grid-cols-1">
+                            <div className="mt-3">
+                                <button className="btn btn-block">Most Sport Played:  {userData.mostSportPlayed}</button>
+                            </div>
                         </div>
-                        <div className="mt-3">
-                            <kbd className="kbd mx-2 text-sm w-60">Type Sport:  <br /> {userData.typeSport}</kbd>
+                        <div className="grid grid-cols-1">
+                            <div className="mt-3">
+                                <button className="btn btn-block">Type Sport:  {userData.typeSport}</button>
+                            </div>
                         </div>
                     </div>
 
@@ -198,7 +203,7 @@ export default function DetailAccount() {
                             (index % 3 === 0) && (
                                 <div className="grid grid-cols-3 mt-2" key={index}>
                                     {statusAchievement.slice(index, index + 3).map((achievement, subIndex) => (
-                                        <button className={`btn btn-active btn-sm ${achievement.status === 'Beginner' ? 'btn-success' : 'btn-error'} me-2 text-xs`} key={subIndex} onClick={() => document.getElementById('my_modal_2').showModal()}>
+                                        <button className={`btn btn-active btn-sm overflow-hidden ${achievement.status === 'Beginner' ? 'btn-success' : 'btn-error'} me-2 text-xs`} key={subIndex} onClick={() => document.getElementById('my_modal_2').showModal()}>
                                             {achievement.name}
                                         </button>
                                     ))}
